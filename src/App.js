d9f9b3cda65d2b7bb98aace294ac8ebc101e9394
edrichmkhasi <edrich@thedigitalacademy.co.za>
2020-04-02 01:15:20 +0200

    just made an application that translates languages

--- src/App.js
+++ src/App.js
@@ -2,22 +2,49 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {useTranslation} from 'react-i18next';
+
 function App() {
+
+  const { t, i18n } = useTranslation();
+
+  function handleClick(lang){
+    i18n.changeLanguage(lang);
+  }
+
   return (
     <div className="App">
+      <nav style={{width: '100%', padding: '2rem 0', backgroundColor: 'grey'}}>
+
+        {/* button for English language */}
+        <button onClick={() => handleClick('en')}>
+          English
+        </button>
+
+        {/* button for Japanese language */}
+        <button onClick={() => handleClick('ko')}>
+          Korean
+        </button>
+
+        {/* button for chinnese language */}
+        <button onClick={() => handleClick('chi')}>
+          Chinnese
+        </button>
+
+
+      </nav>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {t('Programmer.name')}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          {t('Student.name')}
+        </p>
+       
+       
       </header>
     </div>
   );
